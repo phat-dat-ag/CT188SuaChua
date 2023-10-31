de7ce8cb5ec40046e55f01e775e2929913ca8c21
phat-dat-ag <67agphatdat@gmail.com>
2023-10-31 11:27:45 +0700

    Coi như đã hoàn thiện 11h 31/10/23

--- TH_B1_Data/main.js
+++ TH_B1_Data/main.js
@@ -1,15 +1,41 @@
 // Phần thanh tìm kiếm
-
-// Phần form
-//Đăng nhập
-
-// Phần giỏ hàng
-
-// Phần hiện giỏ hàng
 var searchbox = document.getElementById("search");
+var searchInput = searchbox.getElementsByTagName("input")[0];
 var tagis = document.getElementsByTagName("i");
+var searchButton = tagis[0];
 var cart = tagis[1];
+
+searchInput.oninput = function (event) {
+  searchButton.onclick = function () {
+    sendSearch(event.target.value);
+  };
+  searchInput.onkeyup = function (e) {
+    // console.log(e.key == "Enter");
+    if (e.key == "Enter") sendSearch(event.target.value);
+    // if (e.which == 13) {
+    //   sendSearch(event.target.value);
+    // }
+  };
+};
+
+var sendSearch = function (data) {
+  // Cắt bỏ dấu cách bằng trim(), vô nghĩa khi gửi dấu cách đi
+  if (data.trim().length > 0) {
+    //Mô phỏng 1 cái form
+    var form = document.createElement("form");
+    form.action = "timkiem.html";
+    form.method = "get";
+    form.appendChild(searchInput);
+    searchInput.name = "searchInformation";
+    document.body.appendChild(form);
+    form.submit();
+
+    console.log("Gửi dữ liệu đi");
+  } else console.log("Không gửi dữ liệu");
+};
+
 //Mở trang Đơn hàng khi click giỏ hàng
+//Dùng lại biến cart ở phần search
 cart.onclick = function () {
   //   window.open("donhang.html", "_self");
   window.location.href = "donhang.html";
@@ -63,6 +89,7 @@ var itemList = {
   },
 };
 
+//Chuyển object về mảng object cho mình dễ xài
 var hoadon = [];
 var i = 0;
 for (var item in itemList) {
@@ -135,15 +162,15 @@ var showCart = function () {
       tr.appendChild(td_name);
 
       var td_count = document.createElement("td");
-      td_count.innerHTML = `<span>${sp.count}</span>`;
+      td_count.innerHTML = `<span>${sp.count} lốc</span>`;
       tr.appendChild(td_count);
 
       var td_price = document.createElement("td");
-      td_price.innerHTML = `<span>${sp.price}</span>`;
+      td_price.innerHTML = `<span>${sp.price} VNĐ</span>`;
       tr.appendChild(td_price);
 
       var td_total = document.createElement("td");
-      td_total.innerHTML = `<span>${sp.total}</span>`;
+      td_total.innerHTML = `<span>${sp.total} VNĐ</span>`;
       tr.appendChild(td_total);
 
       var td_delete = document.createElement("td");
@@ -151,29 +178,40 @@ var showCart = function () {
       tr.appendChild(td_delete);
 
       //Xóa sản phẩm ra khỏi giỏ hàng
-      td_delete.onclick = function removeCart() {
-        localStorage.removeItem(sp.code);
-        location.reload("donhang.html");
-      };
+      // td_delete.onclick = function removeCart() {
+      //   localStorage.removeItem(sp.code);
+      //   location.reload("donhang.html");
+      // };
 
       //Đưa từng hàng vào body của table
       bodyTable.appendChild(tr);
     }
   });
 
-  //Hiển thị tính tiền ở thẻ tfoot
-  var trA = document.getElementById("trA");
-  trA.innerText = `Thành tiền (A) = ${totalPreTax} đ`;
-  var trB = document.getElementById("trB");
-  trB.innerText = `Chiết Khấu (B) = 0.1xA = ${discount} đ`;
-  var trC = document.getElementById("trC");
-  trC.innerText = `Thuế (C) = 10% x (A-B) = ${tax} đ`;
-  var trD = document.getElementById("trD");
-  trD.innerText = `Tổng cộng (D) = A-B+C = ${finalTotal} đ`;
-};
-document.addEventListener("DOMContentLoaded", function () {
-  showCart();
-});
-//showCart();
+  //Hàm xóa sản phẩm ra khỏi giỏ hàng
+  var delete_buttons = bodyTable.querySelectorAll(".delete_product");
+  // console.log(delete_buttons);
+  delete_buttons.forEach(function (delete_button) {
+    delete_button.onclick = function removeCart() {
+      localStorage.removeItem(delete_button.id);
+      location.reload("donhang.html");
+    };
+  });
 
-console.log(localStorage);
+  //Hiển thị tính tiền ở thẻ tfoot
+  var tdA = document.getElementById("tdA");
+  tdA.innerText = `Thành tiền (A) = ${totalPreTax} VNĐ`;
+  var tdB = document.getElementById("tdB");
+  tdB.innerText = `Chiết Khấu (B) = 0.1xA = ${discount} VNĐ`;
+  var tdC = document.getElementById("tdC");
+  tdC.innerText = `Thuế (C) = 10% x (A-B) = ${tax} VNĐ`;
+  var tdD = document.getElementById("tdD");
+
+  //Có dùng span nên dùng innerHTML thay vì innerText
+  tdD.innerHTML = `Tổng cộng (D) = A-B+C = <span>${finalTotal} VNĐ</span>`;
+};
+
+// Để ở đây không hợp lý, do chỉ cần trang Donhang tải lại là đủ rồi
+// document.addEventListener("DOMContentLoaded", function () {
+//   showCart();
+// });
